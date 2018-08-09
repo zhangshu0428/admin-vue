2fbb8f5d12ab213f7c54e21c7937c8ba01aa2b92
susu <1123844210@qq.com>
2018-08-09 18:41:38 +0800

    home头部完成

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,10 +1,37 @@
 <template>
-    <div>home</div>
+    <el-container class="home-wrap">
+        <el-header class="header">
+            <el-row>
+                <el-col :span="4">
+                    <img src="@/assets/logo.png">
+                </el-col>
+                <el-col :span="19">
+                    <h3>电商后台管理系统</h3>
+                </el-col>
+                <el-col :span="1" class="logout">
+                    <a href="#" @click.prevent="handleLogout">退出</a>
+                </el-col>
+            </el-row>
+        </el-header>
+        <el-container>
+            <el-aside width="200px" class="aside">Aside</el-aside>
+            <el-main class="main">Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    handleLogout() {
+      // 清除token
+      sessionStorage.removeItem('token');
+      // 提示
+      this.$message.success('退出成功');
+      // 重定向到登陆页
+      this.$router.push('/login');
+    }
+  }
 };
 </script>
 
@@ -14,6 +41,20 @@ export default {
     }
     .header {
         background-color: #b3c0d1;
+        padding-left: 10px;
+    }
+    .header h3 {
+        text-align: center;
+        color: #fff;
+        font-size: 24px;
+        line-height: 16px;
+        font-weight: 400;
+    }
+    .header .logout {
+        line-height: 60px;
+    }
+    .header .logout a {
+        color: orange;
     }
     .aside {
         background-color: #d3dce6;
