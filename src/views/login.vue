7af09c7dc3e52b4b3819db517e3f1d41550db4a9
susu <1123844210@qq.com>
2018-08-09 13:49:10 +0800

    尝试登录页

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,15 +1,49 @@
 <template>
     <div>
-        login
+      <div class="login-wrap">
+        <el-form class="login-form" label-position="top" label-width="80px" :model="formData">
+            <el-form-item label="用户名">
+               <el-input v-model="formData.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码">
+                <el-input v-model="formData.password"></el-input>
+            </el-form-item>
+        </el-form>
+        <el-form-item>
+          <el-button class="btn" type="primary">登录</el-button>
+        </el-form-item>
+      </div>
     </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    };
+  }
 };
 </script>
 
 <style>
-
+  .login-wrap {
+    background-color: #324152;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .login-form {
+    background-color: #fff;
+    width: 400px;
+    border-radius: 5px;
+    padding: 30px;
+  }
+  .btn {
+    width: 100%;
+  }
 </style>
