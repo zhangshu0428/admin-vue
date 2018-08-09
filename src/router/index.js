be33b41509c5dd2e208d5bb30336ea106288817c
susu <1123844210@qq.com>
2018-08-09 19:33:20 +0800

    home页面中main的嵌套路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,20 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import login from '@/views/login';
 import home from '@/views/home';
+import users from '@/views/users/users';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     { name: 'login', path: '/login', component: login },
-    { name: 'home', path: '/', component: home }
+    {
+      name: 'home',
+      path: '/',
+      component: home,
+      children: [
+        { path: '/users', component: users }
+      ]
+    }
   ]
 });
