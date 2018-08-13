32e56b1394e8fa40b5ed4ffc5ddc400cb0e3f439
susu <1123844210@qq.com>
2018-08-13 20:16:25 +0800

    商品添加分类完成

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/css/index.css';
+import '@/assets/css/card.css';
 import moment from 'moment';
 import myAxios from '@/plugins/myAxios';
 import MyBreadcrumb from '@/components/MyBreadcrumb';
--- src/views/goods/categories.vue
+++ src/views/goods/categories.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-card>
+  <el-card class="card">
     <!-- 面包屑 -->
     <my-breadcrumb level1="商品管理" level2="商品分类"></my-breadcrumb>
     <el-row>
-        <el-button type="success" plain class="addBtn">添加分类</el-button>
+        <el-button
+        type="success"
+        plain
+        @click="handleAddCat"
+        class="addBtn">添加分类</el-button>
     </el-row>
     <!-- 表格 -->
     <el-table
@@ -58,15 +62,54 @@
               icon="el-icon-delete"
               plain
               size="mini"></el-button>
-              <el-button
-              type="success"
-              icon="el-icon-check"
-              plain
-              size="mini"></el-button>
             </el-row>
           </template>
       </el-table-column>
     </el-table>
+    <!-- 分页-->
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pagenum"
+      :page-sizes="[2, 4, 6, 8]"
+      :page-size="pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
+<!-- 添加分类的弹出框 -->
+    <el-dialog title="添加分类" :visible.sync="addCatDialogFormVisible">
+      <el-form
+        ref="addform"
+        :model="form"
+        label-width="80">
+        <el-form-item label="分类名称">
+          <el-input style="width:400px" v-model="form.cat_name" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="分类列表">
+          <!-- {{ catIds }} -->
+          <!-- <el-select placeholder="默认添加一级分类">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select> -->
+          <!-- 级联选择器
+             v-model="catIds": 选中的分类id,数组形式
+          -->
+          <el-cascader
+             clearable
+             expand-trigger="hover"
+             v-model="catIds"
+             :props="defaultProps"
+             placeholder="默认添加一级分类"
+            :options="options"
+            change-on-select
+          ></el-cascader>
+          </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="addCatDialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="handleSetAddCat">确 定</el-button>
+      </div>
+  </el-dialog>
   </el-card>
 </template>
 
@@ -76,7 +119,32 @@ export default {
   data() {
     return {
       tableData: [],
-      loading: true
+      loading: true,
+      // 添加分类弹出框
+      addCatDialogFormVisible: false,
+      form: {
+        cat_name: ''
+      },
+      // 分页
+      pagenum: 1,
+      pagesize: 4,
+      total: 0,
+      // 级联选择器
+      // 数据源
+      options: [],
+      // 数据源中需要在级联选择器中显示的属性
+      defaultProps: {
+        // 指定选项的值为选项对象的某个属性值
+        value: 'cat_id',
+        // 指定选项标签为选项对象的某个属性值
+        label: 'cat_name',
+        // 指定选项的子选项为选项对象的某个属性值
+        children: 'children'
+      },
+      // 选中分类中的父id
+      catIds: [],
+      currentaPid: 0,
+      currentaLevel: 0
     };
   },
   components: {
@@ -88,12 +156,76 @@ export default {
   methods: {
     // 获取分类数据
     async categoriesList() {
-      const response = await this.$http.get('categories');
+      // 让每一页都显示loading动画
+      this.loading = true;
+      const response = await this.$http.get(`categories?type=3&pagenum=${this.pagenum}&pagesize=${this.pagesize}`);
       this.loading = false;
+      // 响应完成后，获取总条数
+      this.total = response.data.data.total;
       const { meta: { status, msg } } = response.data;
       if (status === 200) {
-        this.tableData = response.data.data;
-        // console.log(this.tableData);
+        this.tableData = response.data.data.result;
+        // console.log(response);
+      } else {
+        this.$message.error(msg);
+      }
+    },
+    handleSizeChange(val) {
+      this.pagesize = val;
+      this.categoriesList();
+    },
+    handleCurrentChange(val) {
+      this.pagenum = val;
+      this.categoriesList();
+    },
+    // 点击添加分类
+    async handleAddCat() {
+      // 显示弹出框
+      this.addCatDialogFormVisible = true;
+      // 发送请求，获取一级和二级数据
+      const response = await this.$http.get('categories?type=2');
+      console.log(response);
+      const { meta: { status, msg } } = response.data;
+      if (status === 200) {
+        // 成功后，将值赋值给options
+        this.options = response.data.data;
+      } else {
+        this.$message.error(msg);
+      }
+    },
+    // 点击添加分类弹出框的确定按钮
+    async handleSetAddCat() {
+      // 关闭弹出框
+      this.addCatDialogFormVisible = false;
+      // 获取三个参数才能发送请求  cat_pid cat_name(this.form.cat_name)  cat_level
+      // cat_pid 通过catIds可以获取
+      // 级别与存放id 的数组关系
+      // this.catIds.length === 0 => cat_level 为0
+      // this.catIds.length === 1 => cat_level 为1
+      // this.catIds.length === 2 => cat_level 为2
+      this.currentaLevel = this.catIds.length;
+      if (this.catIds.length === 1) {
+        // 默认添加一级分类
+        this.currentaPid = this.catIds[0];
+      } else if (this.catIds.length === 2) {
+        this.currentaPid = this.catIds[1];
+      }
+      // 发送请求
+      const response = await this.$http.post('categories', {
+        cat_pid: this.currentaPid,
+        cat_name: this.form.cat_name,
+        cat_level: this.currentaLevel
+      });
+      // console.log(response);
+      const { meta: { status, msg } } = response.data;
+      if (status === 201) {
+        this.$message.success(msg);
+        // 关闭弹出框
+        this.addCatDialogFormVisible = false;
+        // 重新渲染表格
+        this.categoriesList();
+        // 清空添加的弹出框
+        // this.$refs.addform.resetField();
       } else {
         this.$message.error(msg);
       }
