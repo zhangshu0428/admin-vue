bd511541da7a82436aa752f0724da7bce6e90e14
susu <1123844210@qq.com>
2018-08-15 18:43:47 +0800

    商品参数部分基本框架搭建

--- src/views/goods/goodsAdd.vue
+++ src/views/goods/goodsAdd.vue
@@ -25,7 +25,7 @@
     <el-step title="商品内容"></el-step>
   </el-steps>
   <!-- 选项卡 -->
-  <el-form v-model="form" label-width="100px">
+  <el-form v-model="form" label-width="100px" label-position="top">
     <el-tabs tab-position="left" @tab-click="handleTabClick">
       <el-tab-pane label="基本信息">
         <el-form-item label="商品名称">
@@ -41,6 +41,7 @@
           <el-input v-model="form.goods_number"></el-input>
         </el-form-item>
         <el-form-item label="商品分类">
+          {{ catIds }}
           <el-cascader
           clearable
           expand-trigger="hover"
@@ -48,11 +49,23 @@
           :props="defaultProps"
           placeholder="请选择商品分类"
           :options="options"
-          change-on-select
+          @change="handleChange"
         ></el-cascader>
         </el-form-item>
       </el-tab-pane>
-      <el-tab-pane label="商品参数">商品参数</el-tab-pane>
+      <el-tab-pane label="商品参数">
+        <el-form-item :label="item.attr_name" v-for="item in paramData" :key="item.attr_id">
+          <el-checkbox-group>
+            <el-checkbox
+            :label="param"
+            border
+            v-for="param in item.params"
+            :key="param"
+            checked>
+            </el-checkbox>
+          </el-checkbox-group>
+        </el-form-item>
+      </el-tab-pane>
       <el-tab-pane label="商品属性">商品属性</el-tab-pane>
       <el-tab-pane label="商品图片">商品图片</el-tab-pane>
       <el-tab-pane label="商品内容">商品内容</el-tab-pane>
@@ -80,21 +93,66 @@ export default {
       // 数据源中需要在级联选择器中显示的属性
       defaultProps: {
         // 指定选项的值为选项对象的某个属性值
-        value: '',
+        value: 'cat_id',
         // 指定选项标签为选项对象的某个属性值
-        label: '',
+        label: 'cat_name',
         // 指定选项的子选项为选项对象的某个属性值
         children: 'children'
       },
       // 选中分类中的父id
-      catIds: []
+      catIds: [],
+      // 商品参数的数据
+      paramData: []
     };
   },
+  created() {
+    // 获取级联选择器中商品分类数据
+    this.loadData();
+  },
   methods: {
-    // 处理步骤条与选项卡之间的关系
-    handleTabClick(tab, event) {
-      console.log(tab, event);
+    // 处理步骤条与选项卡之间的关系，点击选项卡的时候触发
+    async handleTabClick(tab, event) {
+      // console.log(tab, event);
       this.active = tab.index - 0;
+      // 判断点击的是否是商品参数和商品属性，若是，判断是否选择了三级商品分类
+      if (tab.index === '1' || tab.index === '2') {
+        if (this.catIds.length === 0) {
+          // 提示
+          this.$message.error('请选择商品分类');
+        } else {
+          // 选择了商品分类，发送请求，获取参数数据
+          const response = await this.$http.get(`categories/${this.catIds[2]}/attributes?sel=many`);
+          // console.log(response);
+          const { meta: { status, msg } } = response.data;
+          if (status === 200) {
+            this.paramData = response.data.data;
+            // console.log(this.paramData);
+            // 处理参数类型，把字符串转为数组，动态添加数组
+            this.paramData.map((item) => {
+              const arr = item.attr_vals.length === 0 ? [] : item.attr_vals.split(',');
+              this.$set(item, 'params', arr);
+              // console.log(item);
+            });
+          } else {
+            this.$message.error(msg);
+          }
+        }
+      }
+    },
+    // 获取基本信息中级联选择器中商品分类数据
+    async loadData() {
+      // 发送请求，获取商品分类
+      const response = await this.$http.get('categories?type=3');
+      this.options = response.data.data;
+    },
+    // 基本信息中 文本框的内容变化时触发
+    handleChange() {
+      // 只能添加第三级商品，文本框中只能显示第三级商品
+      if (this.catIds.length !== 3) {
+        this.catIds.length = 0;
+        // 给出提示，只能选择三级分类
+        this.$message.warning('请选择三级分类');
+      }
     }
   }
 };
