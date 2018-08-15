1ccbe4715e48cfabc31f23669040ccee8a71db3f
susu <1123844210@qq.com>
2018-08-15 22:08:41 +0800

    分类参数页面构建完成

--- src/views/goods/params.vue
+++ src/views/goods/params.vue
@@ -14,16 +14,60 @@
       label-width="80">
       <el-form-item label="请选择商品分类">
         <el-cascader
-          clearable
-          expand-trigger="hover"
-          v-model="catIds"
-          :props="defaultProps"
-          placeholder="请选择商品分类"
-          :options="options"
-          change-on-select
-        ></el-cascader>
-        </el-form-item>
-      </el-form>
+        clearable
+        expand-trigger="hover"
+        v-model="catIds"
+        :props="defaultProps"
+        placeholder="请选择商品分类"
+        :options="options"
+        change-on-select
+      ></el-cascader>
+      </el-form-item>
+    </el-form>
+    <!-- tab 栏切换 -->
+    <el-tabs v-model="activeName" @tab-click="handleTabClick">
+      <el-tab-pane label="动态参数" name="first">
+        <!-- 按钮 -->
+        <el-button type="primary">添加动态参数</el-button>
+        <!-- 表格 -->
+        <el-table
+          type="expand"
+          stripe
+          border
+          :data="tableData"
+          style="width: 100%">
+          <el-table-column
+            type="index"
+            width="50">
+          </el-table-column>
+          <el-table-column
+            prop="tableData.attr_name"
+            label="商品参数"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            label="操作">
+            <template slot-scope="scope">
+              <el-row>
+                <el-button
+                type="primary"
+                icon="el-icon-edit"
+                plain
+                size="mini"></el-button>
+                <el-button
+                type="danger"
+                icon="el-icon-delete"
+                plain
+                size="mini"></el-button>
+              </el-row>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-tab-pane>
+      <el-tab-pane label="静态参数" name="second">
+        <el-button type="primary">添加静态参数</el-button>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -33,8 +77,16 @@ export default {
     return {
       catIds: [],
       defaultProps: {},
-      options: []
+      options: [],
+      // tab栏选中的一栏的name
+      activeName: 'first',
+      tableData: []
     };
+  },
+  methods: {
+    // 选项卡切换的时候触发
+    handleTabClick() {
+    }
   }
 };
 </script>
